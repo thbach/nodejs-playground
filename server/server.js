6fa260026063ec0cc75014355f04145b62e73c3b
tbach <thbach@gmail.com>
2018-09-08 00:32:28 -0700

    added tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({todos});
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,19 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todosDummy = [
+  {
+    text: 'Frist text todo',
+  },
+  {
+    text: 'Second text todo',
+  },
+];
+
 mocha.beforeEach(done => {
-  Todo.deleteMany({}).then(() => done());
+  Todo.deleteMany({})
+    .then(() => Todo.insertMany(todosDummy))
+    .then(() => done());
 });
 
 mocha.describe('POST /todos', () => {
@@ -25,7 +36,7 @@ mocha.describe('POST /todos', () => {
           return done(err);
         }
 
-        return Todo.find()
+        return Todo.find({text})
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -46,10 +57,27 @@ mocha.describe('POST /todos', () => {
         }
         return Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(dbErr => done(dbErr));
+      });
+  });
+});
+
+mocha.describe('GET /todos', () => {
+  mocha.it('Should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(err => {
+        if (err) {
+          return done(err);
+        }
+        return done();
       });
   });
 });
