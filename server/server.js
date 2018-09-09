9ff07dacafa6f4d04e573717cf7a897d6c6ee6cb
tbach <thbach@gmail.com>
2018-09-08 23:15:59 -0700

    add get ids

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
     })
     .catch(err => {
       res.status(400).send(err);
+    });
+});
+
+// GET /todos/1233456
+app.get('/todos/:id', (req, res) => {
+  const {id} = req.params;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  return Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send(todo);
+    })
+    .catch(() => {
+      res.status(400).send();
     });
 });
 
