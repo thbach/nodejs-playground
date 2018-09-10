1af2b379599f39e638eb14441e91d9009c948aa5
tbach <thbach@gmail.com>
2018-09-09 23:13:16 -0700

    setup mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${
+  process.env.DB_PASSWORD
+}@ds151382.mlab.com:51382/node-todo-api`;
+
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+  MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
   {useNewUrlParser: true}
 );
 
