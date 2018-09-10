c8b66405cfe86d762a7a8ef0a6d6385be0460fcf
tbach <thbach@gmail.com>
2018-09-10 00:09:53 -0700

    added delete

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${
-  process.env.DB_PASSWORD
-}@ds151382.mlab.com:51382/node-todo-api`;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'production'
+    ? `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds151382.mlab.com:51382/node-todo-api`
+    : 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  'mongodb://localhost:27017/TodoApp' || MONGODB_URI,
+  MONGODB_URI,
   {useNewUrlParser: true}
 );
 
