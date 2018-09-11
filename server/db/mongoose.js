39064af7444f83e967cb1ac5d12bf2068ba05403
tbach <thbach@gmail.com>
2018-09-10 23:47:59 -0700

    added configs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI =
-  process.env.NODE_ENV === 'production'
-    ? `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds151382.mlab.com:51382/node-todo-api`
-    : 'mongodb://localhost:27017/TodoApp';
-
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  MONGODB_URI,
+  process.env.MONGODB_URI,
   {useNewUrlParser: true}
 );
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -7,7 +9,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 8888;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
