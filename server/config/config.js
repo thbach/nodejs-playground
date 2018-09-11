6ce199c60f1180e20b4cfb9ea69819ea0aff5c09
tbach <thbach@gmail.com>
2018-09-10 23:55:27 -0700

    added config

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,8 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 8888;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+  process.env.MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${
+    process.env.DB_PASSWORD
+  }@ds151382.mlab.com:51382/node-todo-api`;
 }
