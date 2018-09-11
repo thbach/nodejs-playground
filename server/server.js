bcee816ca6a8124ff725ef00932728a0c9d46e7b
tbach <thbach@gmail.com>
2018-09-11 00:03:01 -0700

    bug fix config

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
-require('./config/config');
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  process.env.PORT = 8888;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 8888;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+  process.env.MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${
+    process.env.DB_PASSWORD
+  }@ds151382.mlab.com:51382/node-todo-api`;
+}
 
 const express = require('express');
 const bodyParser = require('body-parser');
