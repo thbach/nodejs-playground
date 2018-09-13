e2f1009f81ea8db219155cb94d62ca9cff2c63c9
tbach <thbach@gmail.com>
2018-09-12 23:32:21 -0700

    added GET users/me authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,7 +32,7 @@ const UserSchema = new mongoose.Schema({
     },
   ],
 });
-UserSchema.methods.toJSON = function() {
+UserSchema.methods.toJSON = function toJSON() {
   const user = this;
   const userObject = user.toObject();
   return {
@@ -41,13 +41,30 @@ UserSchema.methods.toJSON = function() {
   };
 };
 
-UserSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function generateAuthToken() {
   const user = this;
   const access = 'auth';
   const token = jwt.sign({_id: user._id.toHexString(), access}, '123abc').toString();
 
   user.tokens = user.tokens.concat({access, token});
   return user.save().then(() => token);
+};
+
+UserSchema.statics.findByToken = function findByToken(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, '123abc');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  });
 };
 
 const User = mongoose.model('Users', UserSchema);
